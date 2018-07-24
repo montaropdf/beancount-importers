be16b9857fde0774401a0f2c6bb938234b99ecfe
Roland Everaert <roland@localhost.localdomain>
2018-07-24 08:47:12 +0200

    First step for improvment

--- importers/smals/__init__.py
+++ importers/smals/__init__.py
@@ -46,11 +46,32 @@ class Importer(importer.ImporterProtocol):
         self.account_vacation = account_vacation
         
         # print('{} {}'.format(self.commodity_overtime, self.commodity_vacation_day))
+
+    def tnx_holiday(self, meta, date, desc, unit_vac, unit_ovt):
+        """Return a holiday transaction object."""
+
+        return  data.Transaction(
+            meta, date, "!", self.employer, desc, data.EMPTY_SET, data.EMPTY_SET, [
+                data.Posting(self.account_vacation, units_vac, None, None, None, None),
+                data.Posting(self.account_employer_holiday, -units_vac, None, None, None, None),
+                data.Posting(self.account_vacation, units_vac, None, unit_ovt, "!", None),
+                data.Posting(self.account_employer_overtime, -unit_ovt, None, None, "!", None)
+                ])
+
+    def tnx_overtime(self, meta, date, unit_ovt):
+        """Return an overtime transaction object."""
+
+        return data.Transaction(
+            meta, date, self.FLAG, self.customer, None, data.EMPTY_SET, data.EMPTY_SET, [
+                data.Posting(self.account_employer_overtime, units_ovt, None, None, None, None),
+                data.Posting(self.account_customer_overtime, -units_ovt, None, None, None, None)
+                ])
         
     def identify(self, file):
         # Match if the filename is as downloaded and the header has the unique
         # fields combination we're looking for.
-        # print("identify")
+        print(file)
+        print("identify")
         return (re.match(r"smals-report-\d\d\d\d\d\d-cleaned.csv", path.basename(file.name)) and
                 re.match("DATE;DAYTYPE;STD;DAYTYPE2;TIMESPENT;DAYTYPE3;TIMEREC", file.head()))
 
@@ -73,6 +94,7 @@ class Importer(importer.ImporterProtocol):
         # print("extract")
         entries = []
         index = 0
+        units_overtime = 0
         swp_minutes = int((self.standard_work_period - datetime.datetime(1970, 1, 1)).total_seconds() / 60)
 
         csvDialect = csv.excel();
@@ -84,6 +106,27 @@ class Importer(importer.ImporterProtocol):
             # print(row['DATE'])
 
             date = datetime.datetime.strptime(row['DATE'], '%d/%m/%Y').date()
+            month = row['DATE'][3:2]
+            print("Month: {}".format(month))
+            print("Current Month: {}".format(cur_month))
+
+            if cur_month == 0:
+                cur_month = month
+
+            # If the month change, create the transaction for the overtime of the previous month
+            if cur_month != month:
+                cur_month = month
+
+                tnx = tnx_overtime(meta, date, units_overtime)
+                # txn = data.Transaction(
+                #     meta, date, self.FLAG, self.customer, None, data.EMPTY_SET, data.EMPTY_SET, [
+                #         data.Posting(self.account_employer_overtime, units_overtime, None, None, None, None),
+                #         data.Posting(self.account_customer_overtime, -units_overtime, None, None, None, None)
+                #     ])
+
+                entries.append(txn)
+                units_overtime = 0
+            
             dtype = row['DAYTYPE']
             dtype2 = row['DAYTYPE2']
             dtype3 = row['DAYTYPE3']
@@ -97,18 +140,28 @@ class Importer(importer.ImporterProtocol):
             # If it is a work day, check if some overtime can be added to the overtime account.
             if dtype2 == "PRE":
                 wk_time = datetime.datetime(1970, 1, 1, int(row['TIMESPENT'].split(':')[0]), int(row['TIMESPENT'].split(':')[1]))
-                overtime = decimal.Decimal((wk_time - self.standard_work_period).total_seconds() / 60)
+                wk_period = self.standard_work_period
 
-                if overtime <= 0:
-                    continue
+                # Check if a part of the day was a vacation
+                if dtype3 == "CAO":
+                    txn = txn_vacation(amount.Amount(decimal.Decimal('0.5'), self.commodity_vacation_day),
+                                       amount.Amount(decimal.Decimal(swp_minutes), self.commodity_overtime))
+                    entries.append(txn)
+                    wk_period = wk_period / 2
+
+                overtime = decimal.Decimal((wk_time - wk_period).total_seconds() / 60)
+
+                # if overtime <= 0:
+                    # continue
 
                 # desc = "Heure Supplémentaire"
-                units_overtime = amount.Amount(overtime, self.commodity_overtime)
-                txn = data.Transaction(
-                    meta, date, self.FLAG, self.customer, None, data.EMPTY_SET, data.EMPTY_SET, [
-                        data.Posting(self.account_employer_overtime, units_overtime, None, None, None, None),
-                        data.Posting(self.account_customer_overtime, -units_overtime, None, None, None, None)
-                    ])
+                units_overtime += amount.Amount(overtime, self.commodity_overtime)
+                # txn = data.Transaction(
+                #     meta, date, self.FLAG, self.customer, None, data.EMPTY_SET, data.EMPTY_SET, [
+                #         data.Posting(self.account_employer_overtime, units_overtime, None, None, None, None),
+                #         data.Posting(self.account_customer_overtime, -units_overtime, None, None, None, None)
+                #     ])
+
             else:
                 # If it is a work day, but I was sick or it was a legal holiday, skip it.
                 if dtype3 in ["JFR", "MAL", "COLFE"]:
@@ -116,39 +169,42 @@ class Importer(importer.ImporterProtocol):
 
                 # If it is a work day, but I was on vacation, add an entry for a vacation day.
                 if dtype3 == "CAO":
-                    desc = "Congé"
-                    units_vacation = amount.Amount(decimal.Decimal('1'), self.commodity_vacation_day)
-                    units_overtime = amount.Amount(decimal.Decimal(swp_minutes), self.commodity_overtime)
-                    txn = data.Transaction(
-                        meta, date, "!", self.employer, desc, data.EMPTY_SET, data.EMPTY_SET, [
-                            data.Posting(self.account_vacation, units_vacation, None, None, None, None),
-                            data.Posting(self.account_employer_holiday, -units_vacation, None, None, None, None),
-                            data.Posting(self.account_vacation, units_vacation, None, units_overtime, "!", None),
-                            data.Posting(self.account_employer_overtime, -units_overtime, None, None, "!", None)
-                        ])
+                    txn = txn_vacation(meta, date, "Congé",
+                                       amount.Amount(decimal.Decimal('1'), self.commodity_vacation_day),
+                                           amount.Amount(decimal.Decimal(swp_minutes), self.commodity_overtime))
+                    entries.append(txn)
+                    
+                    # txn = data.Transaction(
+                    #     meta, date, "!", self.employer, desc, data.EMPTY_SET, data.EMPTY_SET, [
+                    #         data.Posting(self.account_vacation, units_vacation, None, None, None, None),
+                    #         data.Posting(self.account_employer_holiday, -units_vacation, None, None, None, None),
+                    #         data.Posting(self.account_vacation, units_vacation, None, units_overtime, "!", None),
+                    #         data.Posting(self.account_employer_overtime, -units_overtime, None, None, "!", None)
+                    #     ])
                 else:
                     # print("Bad row")
                     continue
 
-            entries.append(txn)
+            # entries.append(txn)
 
-        # Insert a final balance check.
-        # if index:
-        #     entries.append(
-        #         data.Balance(meta, date + datetime.timedelta(days=1),
-        #                      self.account_cash,
-        #                      amount.Amount(D(row['BALANCE']), self.currency),
-        #                      None, None))
+        # When there is no more row to process, create a transaction with the remaining overtime
+        tnx = tnx_overtime(meta, date, units_overtime)
+        # txn = data.Transaction(
+        #     meta, date, self.FLAG, self.customer, None, data.EMPTY_SET, data.EMPTY_SET, [
+        #         data.Posting(self.account_employer_overtime, units_overtime, None, None, None, None),
+        #         data.Posting(self.account_customer_overtime, -units_overtime, None, None, None, None)
+        #         ])
+        entries.append(txn)
 
         return entries
 
 
 def test():
     # Create an importer instance for running the regression tests.
-    importer = Importer("EXTHR", "VACDAY", "7:36", "My Employer s.a.", "Customer s.a."
+    importer = Importer("EXTHR", "VACDAY", "7:36", "My Employer s.a.", "Customer s.a.",
                         "Income:BE:Customer:HeureSup",
                         "Assets:BE:Employer",
                         "Assets:BE:Employer:HeureSup",
                         "Assets:BE:Employer:JourConge",
                         "Expenses:Conge")
-    yield from regression.compare_sample_files(importer, __file__)
+    # yield from regression.compare_sample_files(importer, __file__)
