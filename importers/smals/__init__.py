9722e495d8fc9d84e0498cbe4f78fabc06775710
Roland Everaert <r.everaert@gmail.com>
2018-11-21 20:19:40 +0100

    importers/smals/__init__.py

--- importers/smals/__init__.py
+++ importers/smals/__init__.py
@@ -306,10 +306,15 @@ class Importer(importer.ImporterProtocol):
                 if dtype3 == "CAO":
                     self.logger.info('Half a day was a vacation, record it.')
                     txn = self.__txn_vacation(meta, date, "Demi-jour de congé",
-                                              amount.Amount(decimal.Decimal('0.5'),
-                                                            self.commodity_vacation_day),
-                                              amount.Amount(decimal.Decimal(wk_period),
-                                                            self.commodity_overtime))
+                                              self.__int_or_float_to_Amount(0.5,
+                                                                            self.commodity_vacation_day),
+                                              # amount.Amount(decimal.Decimal('0.5'),
+                                              #               self.commodity_vacation_day),
+                                              self.__int_or_float_to_Amount(wk_period,
+                                                                            self.commodity_overtime),
+                                              # amount.Amount(decimal.Decimal(wk_period),
+                                              #               self.commodity_overtime)
+                    )
                     self.logger.info('Vacation date: %s', date)
                     entries.append(txn)
 
