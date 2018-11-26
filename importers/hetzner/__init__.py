bb54b60f24a0b07f58f941c940e6e65574162515
Roland Everaert <r.everaert@gmail.com>
2018-11-26 19:10:14 +0100

    importers/hetzner/__init__.py

--- importers/hetzner/__init__.py
+++ importers/hetzner/__init__.py
@@ -243,7 +243,8 @@ class Importer(importer.ImporterProtocol):
                 # amt = utils.ifs_to_Amount(row['price_no_vat'], 'EUR')
                 # servers_txn[srv_id]['txn'].append(self.__get_posting(self.account_assets, amt, None))
             else:
-                srv_id = row['description'].split(' ')[1][1:]
+                if re.match('Server #\d{6}'):
+                    srv_id = row['description'].split(' ')[1][1:]
 
             if servers_txn.has_key(srv_id):
                 servers_txn[srv_id]['total'] += row['price_no_vat']
