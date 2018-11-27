ed0d879b874d6b1589e2bc1634c3a441d5eaaeb5
Roland Everaert <r.everaert@gmail.com>
2018-11-27 17:25:32 +0100

    importers/hetzner/__init__.py

--- importers/hetzner/__init__.py
+++ importers/hetzner/__init__.py
@@ -70,6 +70,7 @@ class Importer(importer.ImporterProtocol):
         self.account_liability = account_liability
         self.account_assets = account_assets
         self.policy = policy
+        self.inputFile = InvoiceFileDefinition(self.logger)
         
         self.logger.info("Logger Initialized")
         self.logger.debug("Input parameters:")
