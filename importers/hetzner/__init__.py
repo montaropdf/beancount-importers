de1667e06e02c8a605ff07c91fd2d9e015a356b4
Roland Everaert <r.everaert@gmail.com>
2018-11-28 20:07:27 +0100

    importers/hetzner/__init__.py

--- importers/hetzner/__init__.py
+++ importers/hetzner/__init__.py
@@ -114,7 +114,8 @@ class Importer(importer.ImporterProtocol):
             self.logger.debug("Posting list: %s", str(postings))
 
         if self.policy.posting_policy in [PostingPolicyEnum.MULTI, PostingPolicyEnum.SINGLE]:
-            postings.append(self.__get_posting(self.account_assets, toAmount(-vat, 'EUR')))
+            minus_vat = toAmount("{:.2f}".format(-vat), 'EUR')
+            postings.append(self.__get_posting(self.account_assets, toAmount(minus_vat, 'EUR')))
             
         desc = "Renting of server {} for the period {} to {}".format(srv_id_tag, date_start, date_end)
         txn =  data.Transaction(
