d6e01661e9ed64f4ddc2659fb3aa795880a58c5a
Roland Everaert <r.everaert@gmail.com>
2018-11-28 07:53:03 +0100

    importers/hetzner/policy.py

--- importers/hetzner/policy.py
+++ importers/hetzner/policy.py
@@ -3,12 +3,12 @@ __copyright__ = "Copyright (C) 2018  Roland Everaert"
 __license__ = "GNU GPLv2"
 
 import utils
-from utils import VatBelgiumEnum, EnumPosting
+from utils import VatBelgiumEnum, PostingPolicyEnum
 
 class HetznerPolicy(utils.Policy):
     """A Policy class to set the rules for producing a transaction from Hetzner Invoice."""
     def __init__(self,
-                 posting_policy=EnumPosting.MULTI,
+                 posting_policy=PostingPolicyEnum.MULTI,
                  vat_value=VatBelgiumEnum.VAT21):
 
         self.posting_policy = posting_policy
@@ -16,10 +16,10 @@ class HetznerPolicy(utils.Policy):
         self.validate()
 
     def validate(self):
-        if not isinstance(self.posting_policy, utils.EnumPosting):
+        if not isinstance(self.posting_policy, utils.PostingPolicyEnum):
             raise TypeError
 
-        if not isinstance(self.vat_value, utils.VAT):
+        if not isinstance(self.vat_value, utils.VatBelgiumEnum):
             raise TypeError
 
         
