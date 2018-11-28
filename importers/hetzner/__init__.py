fc48732fc0eb5c6cda16790f3660a83d54e6d681
Roland Everaert <r.everaert@gmail.com>
2018-11-28 22:40:42 +0100

    importers/hetzner/__init__.py

--- importers/hetzner/__init__.py
+++ importers/hetzner/__init__.py
@@ -213,7 +213,7 @@ class Importer(importer.ImporterProtocol):
 
             if srv_id in servers_txn:
                 servers_txn[srv_id]['total'] += (float(row['price_no_vat']) * float(row['qty']))
-                if self.policy.posting_policy == PostingPolicyEnum.MULTI:
+                if self.policy.posting_policy in [PostingPolicyEnum.MULTI, PostingPolicyEnum.MULTI_NO_VAT]:
                     amt = toAmount('-' + row['price_no_vat'], 'EUR')
                     self.logger.debug('Amount: %s', str(amt))
                     servers_txn[srv_id]['txn'].append(self.__get_posting(self.account_assets, amt, None))
