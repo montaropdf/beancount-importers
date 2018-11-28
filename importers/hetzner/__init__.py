acf40fca0af72daf44ec05db3f95379a2072675b
Roland Everaert <r.everaert@gmail.com>
2018-11-28 20:02:13 +0100

    importers/hetzner/__init__.py

--- importers/hetzner/__init__.py
+++ importers/hetzner/__init__.py
@@ -98,12 +98,13 @@ class Importer(importer.ImporterProtocol):
         vat = ((total / 100) * float(self.policy.vat_value))
         postings = []
         if posting_list == None:
-            total_amount = toAmount("{:.2f}".format(total + vat), 'EUR')
-            minus_total_amount = toAmount("{:.2f}".format(-(total + vat)), 'EUR')
-            postings.append(self.__get_posting(self.account_liability, total))
-            postings.append(self.__get_posting(self.account_assets, -total))
+            total_amount = toAmount("{:.2f}".format(total), 'EUR')
+            minus_total_amount = toAmount("{:.2f}".format(-total), 'EUR')
+            postings.append(self.__get_posting(self.account_liability, total_amount))
+            postings.append(self.__get_posting(self.account_assets, minus_total_amount))
             self.logger.debug("Posting list: %s", str(postings))
         else:
+            total_amount = toAmount("{:.2f}".format(total + vat), 'EUR')
             if self.policy.posting_policy == PostingPolicyEnum.SINGLE_INCLUDE_VAT:
                 postings.append(self.__get_posting(self.account_liability, toAmount("{:.2f}".format(total + vat), 'EUR')))
             else:
