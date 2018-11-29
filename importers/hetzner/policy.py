7cfcf25ca624c9790b34cc2e003098bc74de2165
Roland Everaert <r.everaert@gmail.com>
2018-11-29 07:59:17 +0100

    importers/hetzner/policy.py

--- importers/hetzner/policy.py
+++ importers/hetzner/policy.py
@@ -2,7 +2,6 @@
 __copyright__ = "Copyright (C) 2018  Roland Everaert"
 __license__ = "GNU GPLv2"
 
-import utils
 from utils import VatBelgiumEnum, PostingPolicyEnum
 
 class HetznerPolicy(utils.Policy):
@@ -16,10 +15,10 @@ class HetznerPolicy(utils.Policy):
         self.validate()
 
     def validate(self):
-        if not isinstance(self.posting_policy, utils.PostingPolicyEnum):
+        if not isinstance(self.posting_policy, PostingPolicyEnum):
             raise TypeError
 
-        if not isinstance(self.vat_value, utils.VatBelgiumEnum):
+        if not isinstance(self.vat_value, VatBelgiumEnum):
             raise TypeError
 
         
